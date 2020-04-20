6404d34d513ca8f572a276dda076639388c7070a
chenchen.ccqy66 <chenchen.ccqy66@bytedance.com>
2020-04-20 17:24:06 +0800

    update gitbook

---
+++ _book/publish.sh
@@ -0,0 +1,16 @@
+git checkout build
+gitbook init
+gitbook install
+gitbook build
+git add .
+git commit -m 'update gitbook'
+git push origin build
+git checkout master
+rm -rf *
+git checkout build -- _book
+mv _book/* ./
+rm -rf _book
+rm -rf publish.sh
+git add .
+git commit -m 'publish gitbook'
+git push origin master
